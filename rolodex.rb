c1564db9572efe77ec7ecf06dce71b8d15a8554b
bshain <shainbarbara@gmail.com>
2014-01-21 01:42:44 -0500

    search function added-- not pretty, but it works.

--- rolodex.rb
+++ rolodex.rb
@@ -30,6 +30,11 @@ class Rolodex
     puts "Name: #{contact.first_name} #{contact.last_name}"
     puts "ID: #{contact.id}"
     puts
+    go_back
+  end
+
+  def go_back
+    puts   
     puts  "Press enter to perform another action."
     ok = gets.chomp
   end
@@ -47,17 +52,49 @@ class Rolodex
     puts "Enter a number:"
     user_search = gets.chomp.to_i
     
-    puts "Which name would you like to look up?" if user_search == 1
-    search_name = gets.chomp.capitalize
+    if user_search == 1
+      puts "\e[H\e[2J"
+      puts "Which first name would you like to look up?" 
+      search_name = gets.chomp.capitalize
+      @contacts.each do |contact|
+        if search_name == contact.first_name
+          display_contact(contact)
+        elsif search_name != contact.first_name
+          puts "Sorry, that first name isn't in the database."
+          go_back
+        end
+      end 
 
+    elsif user_search == 2
+      puts "\e[H\e[2J"
+      puts "Which last name would you like to look up?" 
+      search_surname = gets.chomp.capitalize
+      @contacts.each do |contact|
+        if search_surname == contact.last_name
+          display_contact(contact)
+        elsif search_surname != contact.last_name
+          puts "Sorry, that last name isn't in the database."
+          go_back  
+        end
+      end 
 
-    @contacts.each do |contact|
-      if search_name == contact.first_name
-        display_contact(contact)
-      end
-    end 
+    elsif user_search == 3
+      puts "\e[H\e[2J"
+      puts "Which ID would you like to look up?" 
+        search_id = gets.chomp.capitalize.to_i
+        @contacts.each do |contact|
+          if search_id == contact.id
+            display_contact(contact)
+          elsif search_id != contact.id
+            puts "Sorry, that name isn't in the database."
+            go_back  
+          end
+        end 
+
+     else
+        puts "That is not a valid selection."   
+    end
   end
-
 
 
 
