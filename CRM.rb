2ff115621fe06755758d30f55fcbcf31ba0d7815
bshain <shainbarbara@gmail.com>
2014-01-21 02:03:33 -0500

    changed while loop to unless loop to accomodate exit method in rolodex.rb

--- CRM.rb
+++ CRM.rb
@@ -33,10 +33,12 @@ class CRM
   end
 
   def run
-    while user_selected!=6
+    unless user_selected==6
       print_main_menu
       user_selected = gets.chomp.to_i
       call_option(user_selected)
+     else
+      call_option(user_selected) 
     end
   end
 
